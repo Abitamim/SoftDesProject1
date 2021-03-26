a1b11fd914c86588664916ee4a4f48ff39743192
Dasha Chadiuk <dchadiuk@olin.edu>
2021-03-26 16:10:34 -0400

    finished ScrapeRussiaElectionData.py

--- ScrapeRussiaElectionData.py
+++ ScrapeRussiaElectionData.py
@@ -6,19 +6,33 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from os import stat
 from bs4 import BeautifulSoup
+import re
 
-
-def get_vote_counts(driver, page_html):
+def get_vote_counts(page_html:str) -> str:
     '''
     docstring here
 
     Args: 
     Returns: 
     '''
-    
-    pass
 
-def save_csv(votes_data, path): 
+    soup = BeautifulSoup(page_html)
+    tables = soup.find_all('table')
+    rows = tables[-2].find_all('tr')[13:]
+
+    candidates_and_votes = [r.text.split('\n')[1][2:] for r in rows]
+    separate_candidate_votes_regex = re.compile("([^0-9]+)([0-9]+)")
+    candidates_and_votes = [separate_candidate_votes_regex.match(cav).groups() for cav in candidates_and_votes]
+
+    location = tables[1].find_all('tr')[0].find('td').text.split(' > ')
+    region_oblast = ",".join([location[1], location[2][:-2]])
+
+    oblast_csv = '\n'.join([','.join([cav[0],cav[1],region_oblast]) for cav in candidates_and_votes])
+
+    return oblast_csv
+
+
+def save_csv(votes_data, path):
     '''
     Adds a string of data to the end of a csv file.
 
@@ -29,10 +43,11 @@ def save_csv(votes_data, path):
         the data
     '''
     file = open(path, 'a')
-    if stat(path).st_size == 0: 
-        file.write('candidate, votes, city, oblast\n')
+    if stat(path).st_size == 0:
+        file.write('candidate, votes, region, oblast\n')
     file.write(votes_data)
     file.close()
+
 
 def get_election_data():
     '''
@@ -41,42 +56,48 @@ def get_election_data():
 
     url = 'http://www.vybory.izbirkom.ru/region/izbirkom?action=show&root_a=null&vrn=100100084849062&region=0&global=true&type=0&prver=0&pronetvd=null'
 
-    #Using Chrome version 89 and chromedriver version 89 (important that they match)
+    # Using Chrome version 89 and chromedriver version 89 (important that they match)
     driver = webdriver.Chrome('/home/softdes/Downloads/chromedriver')
 
     driver.get(url)
-    #20 seconds to manually enter code to proceed
-    time.sleep(20)
+    # 20 seconds to manually enter code to proceed
+    time.sleep(10)
 
-    #wait until page loads, then select the page with the table of data
-    #only need to do this once as the configurations save
-    table_format = WebDriverWait(driver,10).until(
-        EC.presence_of_element_located((By.LINK_TEXT,'Результаты выборов'))
+    # wait until page loads, then select the page with the table of data
+    # only need to do this once as the configurations save
+    table_format = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, 'Результаты выборов'))
     )
 
     table_format.click()
 
-    dropdown_cities = driver.find_element_by_name('gs')
-    election_cities = dropdown_cities.find_elements_by_tag_name('options')
-    for cities in election_cities:
-        #navigate to the page with data for the city
-        driver.find_element_by_link_text(f'{cities}').click()
-        select_button = driver.find_elements_by_name('go')
+    dropdown_regions = driver.find_element_by_name('gs')
+    election_regions = dropdown_regions.find_elements_by_tag_name('option')
+    for k in range(1,len(election_regions)):
+        dropdown_regions = driver.find_element_by_name('gs')
+        election_regions = dropdown_regions.find_elements_by_tag_name('option')
+        # navigate to the page with data for the region
+        election_regions[k].click()
+        select_button = driver.find_element_by_name('go')
         select_button.click()
-        
+
         dropdown_oblast = driver.find_element_by_name('gs')
-        election_oblast = dropdown_oblast.find_element_by_tag_name('options')
-        
-        for oblast in election_oblast:
-            #navigate to the page for an oblast in that city
-            driver.find_element_by_link_text(f'{oblast}').click()
+        election_oblast = dropdown_oblast.find_elements_by_tag_name('option')
+
+        for i in range(1,len(election_oblast)):
+            dropdown_oblast = driver.find_element_by_name('gs')
+            election_oblast = dropdown_oblast.find_elements_by_tag_name('option')
+            # navigate to the page for an oblast in that city
+            election_oblast[i].click()
+            select_button = driver.find_element_by_name('go')
             select_button.click()
-            #oblast_data = get_vote_counts(driver, driver.page_source)
-            oblast_data =''
+            oblast_data = get_vote_counts(driver.page_source)
             save_csv(oblast_data, 'data/2018-Russia-election-data.txt')
             driver.back()
+        driver.back()
 
     driver.quit()
+
 
 if __name__ == '__main__':
     get_election_data()
