a8fd1bc862ab7f9212dd692e4e531dc1404603a2
Dasha Chadiuk <dchadiuk@olin.edu>
2021-04-03 12:36:41 -0400

    Modified the functions in data_analysis.py
    Added condition to find_all_leading_digits that removes any leadings
    digits that are 0. The data_to_percentage now returns a list of all
    percentages for the numbers 1-9 instead of the percentage for a single
    digit.

--- data_analysis.py
+++ data_analysis.py
@@ -5,7 +5,8 @@ Contains helper functions for analyzing the election data.
 import pandas as pd
 from collections import Counter
 
-def csv_to_dataframe(csv_filepath:str):
+
+def csv_to_dataframe(csv_filepath: str):
     '''
     Takes a csv file and turns it into a dataframe.
 
@@ -17,44 +18,39 @@ def csv_to_dataframe(csv_filepath:str):
     '''
     return pd.read_csv(f'{csv_filepath}')
 
-def find_all_leading_digits(dataframe, leading_digit:int, column_name:str):
+
+def find_all_leading_digits(dataframe, leading_digit: int, column_name: str):
     '''
     Takes data from a given csv file and finds all of the specified leading
     digits in the given column and returns the leadings digits in a list. Note
     that any leading digits that are 0 will be removed as Benford's law does not
-    apply to 0s.
+    apply to 0s, thus only values 1-9 will be returned.
 
     Args: 
         dataframe: a dataframe containing the election data
         leading_digit: an integer representing which leading digit to grab, such
         as the 1st leading digit.
         column_name: a string representing the column in the csv file that
-        contains the integers to find 
-        the leading digit of. 
+        contains the integers to find the leading digit of. 
     Returns: 
         A list of integers containing all leading digits in the given column of
         the csv file. 
     '''
-    #should we add something that checks if that digit does 
-    #not exist in some numbers and ask the person to retry wth a different number? 
+    return [int(str(vote)[leading_digit-1]) for vote in dataframe[f'{column_name}'] if int(str(vote)[leading_digit-1]) != 0]
 
-    return [int(str(vote)[leading_digit-1]) for vote in dataframe[f'{column_name}']]
-    
-
-def data_to_percentage(data_list, number:int):
+def data_to_percentage(data_list):
     '''
-    Takes a list of integer data and an integer, and determines what percent of
-    the data is that integer. For this project, the data_list is the list of
-    leading digits, and the number is between 1-9.
+    Takes a list of integer data and returns a list of percentages corresponding
+    to the number of times the numbers 1-9 appear in the list.
 
     Args: 
         data_list: a list of integers representing all of the leading digits
         from a dataset (in this case, the number of vote counts). 
-        number: an integer representing a number between 1-9.
-    
+
     Returns: 
-        returns a float representing the percentage of that data that is
-        the given number.
+        returns a list of floats representing the percentages of that data that are
+        the numbers 1-9.
     '''
-    number_of_occurances = Counter(data_list)
-    return (number_of_occurances[number]/len(data_list))*100
+    number_of_occurrences = Counter(data_list)
+    return [(number_of_occurrences[number]/len(data_list))*100 for number in range(1, 9)]
+
