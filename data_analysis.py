c61f1941dcd28de07c284e9b8206681de20543eb
Abitamim <abitamimbharmal@hotmail.com>
2021-04-03 12:38:51 -0500

    Changed lists to Series
    Modified code to use Series instead of lists for easier analysis and
    plotting.

--- data_analysis.py
+++ data_analysis.py
@@ -1,13 +1,12 @@
-'''
+"""
 Contains helper functions for analyzing the election data.
-'''
+"""
 
 import pandas as pd
-from collections import Counter
 
 
 def csv_to_dataframe(csv_filepath: str):
-    '''
+    """
     Takes a csv file and turns it into a dataframe.
 
     Args: 
@@ -15,12 +14,12 @@ def csv_to_dataframe(csv_filepath: str):
         all of the election data.
     Returns:
         a dataframe containing all of the data from a given csv file. 
-    '''
-    return pd.read_csv(f'{csv_filepath}')
+    """
+    return pd.read_csv(f"{csv_filepath}")
 
 
 def find_all_leading_digits(dataframe, leading_digit: int, column_name: str):
-    '''
+    """
     Takes data from a given csv file and finds all of the specified leading
     digits in the given column and returns the leadings digits in a list. Note
     that any leading digits that are 0 will be removed as Benford's law does not
@@ -35,11 +34,18 @@ def find_all_leading_digits(dataframe, leading_digit: int, column_name: str):
     Returns: 
         A list of integers containing all leading digits in the given column of
         the csv file. 
-    '''
-    return [int(str(vote)[leading_digit-1]) for vote in dataframe[f'{column_name}'] if int(str(vote)[leading_digit-1]) != 0]
+    """
+    return pd.Series(
+        [
+            int(str(vote)[leading_digit - 1])
+            for vote in dataframe[f"{column_name}"]
+            if int(str(vote)[leading_digit - 1]) != 0
+        ]
+    )
 
-def data_to_percentage(data_list):
-    '''
+
+def data_to_percentage(data_list: pd.Series):
+    """
     Takes a list of integer data and returns a list of percentages corresponding
     to the number of times the numbers 1-9 appear in the list.
 
@@ -48,9 +54,11 @@ def data_to_percentage(data_list):
         from a dataset (in this case, the number of vote counts). 
 
     Returns: 
-        returns a list of floats representing the percentages of that data that are
-        the numbers 1-9.
-    '''
-    number_of_occurrences = Counter(data_list)
-    return [(number_of_occurrences[number]/len(data_list))*100 for number in range(1, 9)]
-
+        returns a list of floats representing the percentages of that data that 
+        are the numbers 1-9.
+    """
+    number_of_occurrences = data_list.value_counts()
+    number_of_occurrences = number_of_occurrences[
+        (number_of_occurrences.index > 0) & (number_of_occurrences.index < 10)
+    ]
+    return number_of_occurrences.multiply(100 / sum(number_of_occurrences))
