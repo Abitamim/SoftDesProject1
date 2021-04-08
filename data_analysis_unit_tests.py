d1be588b51fe9579e07f886f679c1361259735fc
Abitamim <abitamimbharmal@hotmail.com>
2021-04-08 11:26:14 -0500

    Add tests for getting theoretical benford values
    Added unit tests for get_theoretical_benford_law_values.

--- data_analysis_unit_tests.py
+++ data_analysis_unit_tests.py
@@ -2,34 +2,26 @@
 Test library functions to find and identify protein-coding genes in DNA.
 """
 import pytest
+import pandas as pd
+import numpy as np
 
 from data_analysis import (
-    get_complement,
+    get_theoretical_benford_law_values,
 )
 
 
 # Define sets of test cases.
-get_complement_cases = [
-    # Check that the complement of A is T.
-    ("A", "T"),
-    # Check that the complement of C is G.
-    ("C", "G"),
-    # Check that the complement of T is A.
-    ("T", "A"),
-    # Check that the complement of G is C.
-    ("G", "C"),
+get_ideal_benfords= [
+    # Check that default parameter value works.
+    ([], pd.Series(np.array([30.1, 17.609, 12.494, 9.691, 7.918, 6.694, 5.799, 5.115, 4.575]), index=np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]))),
+    # Check that explicit parameter value works
+    ([15], pd.Series(np.array([30.103, 21.388, 16.633, 13.622, 11.539, 10.012, 8.842, 7.918, 7.169, 6.55, 6.03, 5.586, 5.203, 4.869, 4.576]), index=np.array([1, 1.571, 2.143, 2.714, 3.286, 3.857, 4.429, 5, 5.571, 6.143, 6.714, 7.286, 7.857, 8.429, 9]))),
+    # Check that a value under 9 gives an error
+    ([8], pd.Series(np.array([30.1, 17.609, 12.494, 9.691, 7.918, 6.694, 5.799, 5.115, 4.575]), index=np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]))),
 ]
 
 # Define additional testing lists and functions that check other properties of
 # functions in gene_finder.py.
-@pytest.mark.parametrize("nucleotide", ["A", "T", "C", "G"])
-def test_double_complement(nucleotide):
-    """
-    Check that taking the complement of a complement of a nucleotide produces
-    the original nucleotide.
-
-    Args:
-        nucleotide: A single-character string representing one of the four DNA
-            nucleotides.
-    """
-    assert get_complement(get_complement(nucleotide)) == nucleotide
+@pytest.mark.parametrize("input,output", get_ideal_benfords)
+def test_get_ideal_benfords(input, output):
+    assert get_theoretical_benford_law_values(*input).index.tolist() == pytest.approx(output.index.tolist(), .01) and get_theoretical_benford_law_values(*input).to_list() == pytest.approx(output.to_list(), .01) 
