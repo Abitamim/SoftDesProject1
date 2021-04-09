53db7ebb0742edbdc2bc55cf8e48f47dc7645e97
Dasha Chadiuk <dchadiuk@olin.edu>
2021-04-09 16:11:42 -0400

    fixed pylint issue

--- quick_fix_too_many_commas.py
+++ quick_fix_too_many_commas.py
@@ -1,13 +1,17 @@
+"""
+Fixes the extra commas in some of the regions in the Russian data.
+"""
+
 file = open("data/2018-Russia-election-data.csv", "r", encoding="utf-8")
-text = ""
+TEXT = ""
 for line in file:
-    if len(line.split(',')) == 5:
-        last_char_index = line.rfind(',')
-        new_line=(line[:last_char_index] + '' + line[last_char_index + 1:])
-        text += new_line
+    if len(line.split(",")) == 5:
+        last_char_index = line.rfind(",")
+        new_line = line[:last_char_index] + "" + line[last_char_index + 1:]
+        TEXT += new_line
     else:
-        text+=(line)
+        TEXT += line
 file.close()
 x = open("data/2018-Russia-election-data.csv", "w", encoding="utf-8")
-x.writelines(text)
+x.writelines(TEXT)
 x.close()
